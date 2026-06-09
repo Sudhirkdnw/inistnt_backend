cdf325ec06a48cdc72eb59d2e20773d15ee79e6a
sudhirkdnw <sudhirkdnw@gmail.com>
2026-06-09 19:59:25 +0530

    fix: restore missing getSettings & updateSetting in admin exports

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -1950,6 +1950,7 @@ module.exports = {
     getPendingVerifications, handleVerification,
     getAllDatingProfiles, handleDatingProfile,
     getAuditLogs,
+    getSettings, updateSetting,
     flushRedis, resetAllPasswords, broadcastAnnouncement, broadcastPushNotification, getGlobalNotificationHistory,
     uploadSystemAsset,
     getEmailLogs, getEmailTemplates, updateEmailTemplate, sendTestEmail,
