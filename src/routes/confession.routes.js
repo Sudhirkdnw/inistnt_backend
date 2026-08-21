1966c5fc7f73ac31f2ee280c6065041ca98955ce
sudhirkdnw <sudhirkdnw@gmail.com>
2026-08-21 23:51:09 +0530

    photo feature

--- src/routes/confession.routes.js
+++ src/routes/confession.routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const router = express.Router();
 const { authMiddleware } = require('../middlewares/authmiddleware');
 const cacheMiddleware = require('../middlewares/cacheMiddleware');
 const multer = require('multer');
